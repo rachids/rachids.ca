6ff9d70dfeb5a6b54b42a8b4bd49e63dab899506
rachid <rachid@folkshr.com>
2021-10-02 20:03:14 +0200

    Fix failing test

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -3,11 +3,21 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class HomeControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('wink:install');
+        config(['wink.database_connection' => 'sqlite']);
+        $this->artisan('wink:migrate');
+    }
+
     /**
      * A basic feature test example.
      *
@@ -15,6 +25,7 @@ class HomeControllerTest extends TestCase
      */
     public function test_it_loads_the_home_page()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get(route('home.index'));
 
         $response->assertStatus(200);
