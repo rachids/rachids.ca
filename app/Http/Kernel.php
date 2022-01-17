7cd971efc735882ba1a9fa51575171aa289330a5
rachids <livecoding@rachids.ca>
2022-01-16 19:03:34 -0500

    Ajout d'autres headers de sécurité

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\SecurityHeaders;
 use App\Http\Middleware\HttpRedirectSecure;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -23,6 +24,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         HttpRedirectSecure::class,
+        SecurityHeaders::class,
     ];
 
     /**
