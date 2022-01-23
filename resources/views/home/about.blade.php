1b3b5d22b298d91cb5b0d12238930b2209ded6ef
rachids <livecoding@rachids.ca>
2022-01-22 20:24:15 -0500

    Ajout de la page A propos

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,25 @@
+<x-layouts.landing>
+    <h1 class="mb-4 text-2xl tracking-tight font-extrabold sm:text-3xl md:text-4xl">
+        <span class="xl:inline">Qui est</span>
+        <span class="text-blue-400 xl:inline">rachids</span>
+    </h1>
+
+    <p class="mt-2">
+        Je m'appelle Rachid, je travaille avec PHP depuis plus de 20 ans. <br/>
+        J'ai eu le temps de bien voir les évolutions de ce langage, notamment dans les dernières années. <br/>
+        Depuis 2015 je m'efforce d'étendre mes connaissances sur des sujets tels que:
+    </p>
+
+    <ul class="mt-2 list-disc ml-5 list-inside">
+        <li>le TDD : le développement piloté par les tests</li>
+        <li>la qualité du code</li>
+        <li>le framework Laravel</li>
+        <li>les pratiques d'intégration continue et de livraison continue <em>(CI/CD)</em></li>
+    </ul>
+
+    <p class="mt-2">
+        Suivez mon cheminement à travers <x-ui.link href="{{ route('blog.index') }}">mon blog</x-ui.link>,
+        <x-ui.link href="https://twitter.com/rachid_in">mon fil Twitter</x-ui.link> ainsi que
+        <x-ui.link href="https://www.youtube.com/channel/UCsmtNx-781JBVIDw6bNkKvw">mes capsules vidéos sur Youtube</x-ui.link>.
+    </p>
+</x-layouts.landing>
